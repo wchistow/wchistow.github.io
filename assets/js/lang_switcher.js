30e2441e56738a4ae1f7df01f5973b7c7e079ebe
Vladimir Chistov <wchistow@yandex.ru>
2023-07-30 10:23:33 +0300

    move language switching to file

---
+++ assets/js/lang_switcher.js
@@ -0,0 +1,8 @@
+$('document').ready(function() {
+    $('select #en').click(function(evt) {
+        window.open('https://wchistow.github.io/en', '_self');
+    });
+    $('select #ru').click(function(evt) {
+        window.open('https://wchistow.github.io/ru', '_self');
+    });
+});
