cf3f8b08d386bdd278012628161ca958ed3103b4
Vladimir Chistov <wchistow@yandex.ru>
2023-07-31 12:03:31 +0300

    add `projects/` url

--- assets/js/lang_switcher.js
+++ assets/js/lang_switcher.js
@@ -1,8 +1,15 @@
+function switchLangFromRuToEn() {
+    var curUrl = document.location.href
+    document.location.href = curUrl.replace('wchistow.github.io/ru', 'wchistow.github.io/en')
+}
+
+function switchLangFromEnToRu() {
+    var curUrl = document.location.href
+    document.location.href = curUrl.replace('wchistow.github.io/en', 'wchistow.github.io/ru')
+}
+
+
 $('document').ready(function() {
-    $('select #en').click(function(evt) {
-        window.open('https://wchistow.github.io/en', '_self');
-    });
-    $('select #ru').click(function(evt) {
-        window.open('https://wchistow.github.io/ru', '_self');
-    });
-});
+    $('select #en').click(switchLangFromRuToEn)
+    $('select #ru').click(switchLangFromEnToRu)
+})
