1d0a09a6d970fd80e26b0e675a09e2b49e69d695
Vladimir Chistov <wchistow@yandex.ru>
2023-08-02 12:13:11 +0300

    fix bug with language selecting

--- assets/js/lang_switcher.js
+++ assets/js/lang_switcher.js
@@ -1,3 +1,13 @@
+function setUp() {
+    var url = document.location.href
+    var select = $('div#lang-choosing select')
+    if (url.includes('wchistow.github.io/ru')) {
+        select.val('ru').change()
+    } else if (url.includes('wchistow.github.io/en')) {
+        select.val('en').change()
+    }
+}
+
 function switchLangFromRuToEn() {
     var curUrl = document.location.href
     document.location.href = curUrl.replace('wchistow.github.io/ru', 'wchistow.github.io/en')
@@ -10,6 +20,7 @@ function switchLangFromEnToRu() {
 
 
 $('document').ready(function() {
+    setUp()
     $('select #en').click(switchLangFromRuToEn)
     $('select #ru').click(switchLangFromEnToRu)
 })
