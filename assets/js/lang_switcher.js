8c41e41a97c78af226ed129714963eece6f5d648
Vladimir Chistov <wchistow@yandex.ru>
2023-10-24 14:08:05 +0300

    fix language switching

--- assets/js/lang_switcher.js
+++ assets/js/lang_switcher.js
@@ -1,6 +1,6 @@
 function setUp() {
-    var url = document.location.href
-    var select = $('div#lang-choosing select')
+    let url = document.location.href
+    let select = $('div#lang-choosing select')
     if (url.includes('wchistow.github.io/ru')) {
         select.val('ru').change()
     } else if (url.includes('wchistow.github.io/en')) {
@@ -8,19 +8,16 @@ function setUp() {
     }
 }
 
-function switchLangFromRuToEn() {
-    var curUrl = document.location.href
-    document.location.href = curUrl.replace('wchistow.github.io/ru', 'wchistow.github.io/en')
+function changeLang() {
+    let curUrl = document.location.href
+    if ($(this).val() === 'en') {
+        document.location.href = curUrl.replace('wchistow.github.io/ru', 'wchistow.github.io/en')
+    } else {
+        document.location.href = curUrl.replace('wchistow.github.io/en', 'wchistow.github.io/ru')
+    }
 }
-
-function switchLangFromEnToRu() {
-    var curUrl = document.location.href
-    document.location.href = curUrl.replace('wchistow.github.io/en', 'wchistow.github.io/ru')
-}
-
 
 $('document').ready(function() {
     setUp()
-    $('select #en').click(switchLangFromRuToEn)
-    $('select #ru').click(switchLangFromEnToRu)
+    $('select').change(changeLang)
 })
